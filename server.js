b83749628b52a901d6c0ad5b4c25c35eb896da6d
Alex Waterson-Rosso <89030416+alexwatersonrosso@users.noreply.github.com>
2022-04-27 15:35:36 -0500

    Delete server.js

--- server.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const users = require('./data/users');
-
-const app = express();
-
-const PORT = 3001;
-app.get('/api/users', (req, res) => res.json(users));
-
-app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
